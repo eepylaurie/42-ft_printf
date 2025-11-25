0bc1044d842d0a2989991302d81a402c359d8cc1
eepylaurie <eepylaurie@gmail.com>
2025-11-25 19:46:58 +0100

    Add header file for ft_printf with function declarations

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 19:40:37 by lmatthes          #+#    #+#             */
+/*   Updated: 2025/11/25 19:46:24 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putptr(unsigned long p);
+int	ft_putnbr(int n);
+int	ft_putuint(unsigned int n);
+int	ft_puthex(unsigned int n, char format);
+
+#endif
