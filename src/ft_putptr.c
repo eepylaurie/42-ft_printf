bd4d974b9f131677a086eca39fa090310b130fa6
eepylaurie <eepylaurie@gmail.com>
2025-11-25 21:40:06 +0100

    Add ft_putptr_hex function for hexadecimal conversion in ft_putptr

--- src/ft_putptr.c
+++ src/ft_putptr.c
@@ -6,11 +6,28 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 21:26:39 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/11/25 21:32:34 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/11/25 21:39:30 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_putptr_hex(unsigned long n)
+{
+	char	*base;
+	int		count;
+
+	base = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+	{
+		count += ft_putptr_hex(n / 16);
+		count += ft_putptr_hex(n % 16);
+	}
+	else
+		count += ft_putchar(base[n]);
+	return (count);
+}
 
 int	ft_putptr(unsigned long p)
 {
