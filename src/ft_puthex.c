73145f776bafa646bb4e300a1087f32db267d087
eepylaurie <eepylaurie@gmail.com>
2025-11-25 21:32:50 +0100

    Add ft_puthex function for hexadecimal output in ft_printf

---
+++ src/ft_puthex.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 21:14:42 by lmatthes          #+#    #+#             */
+/*   Updated: 2025/11/25 21:25:22 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_puthex(unsigned int n, char format)
+{
+	char	*base;
+	int		count;
+
+	if (format == 'X')
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+	{
+		count += ft_puthex(n / 16, format);
+		count += ft_puthex(n % 16, format);
+	}
+	else
+		count += ft_putchar(base[n]);
+	return (count);
+}
