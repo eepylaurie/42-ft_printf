eb942053caeb90b77ef54880fa12ff2739c092b0
eepylaurie <eepylaurie@gmail.com>
2025-11-25 19:55:42 +0100

    Refactor ft_process_specifier to handle format specifiers in ft_printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,31 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 23:42:35 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/11/25 19:48:34 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/11/25 19:54:00 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_process_specifier(const char c, va_list *args)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(*args, int)));
+	else if (c == 's')
+		return (ft_putstr(va_arg(*args, char *)));
+	else if (c == 'p')
+		return (ft_putptr(va_arg(*args, unsigned long)));
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(*args, int)));
+	else if (c == 'u')
+		return (ft_putunsigned(va_arg(*args, unsigned int)));
+	else if (c == 'x' || c == 'X')
+		return (ft_puthex(va_arg(*args, unsigned int), c));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	else
+		return (0);
+}
 
 int	ft_printf(const char *format, ...)
 {
