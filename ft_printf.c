08afcedaa9150b73d03083aa9f3b6fbaacf7fea4
lmatthes <lmatthes@student.42heilbronn.de>
2026-07-25 20:10:07 +0200

    build: set up ft_printf skeleton with literal output

---
+++ ft_printf.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/25 20:03:21 by lmatthes          #+#    #+#             */
+/*   Updated: 2026/07/25 20:03:25 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count;
+	int		i;
+
+	va_start(args, format);
+	count = 0;
+	i = 0;
+	while (format[i])
+	{
+		count += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/25 20:02:59 by lmatthes          #+#    #+#             */
+/*   Updated: 2026/07/25 20:03:02 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+
+#endif
