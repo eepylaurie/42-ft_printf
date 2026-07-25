9d56970230354d5211fe0df60d8d89fc8964673b
lmatthes <lmatthes@student.42heilbronn.de>
2026-07-25 20:20:01 +0200

    feat: add %u conversion

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 20:02:59 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/07/25 20:14:31 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/07/25 20:18:43 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(int n);
 int	ft_format(char specifier, va_list args);
+int	ft_putnbr_unsigned(unsigned int n);
 
 #endif
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 20:04:52 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/07/25 20:16:57 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/07/25 20:19:00 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ int	ft_format(char specifier, va_list args)
 		return (ft_putchar('%'));
 	else if (specifier == 'd' || specifier == 'i')
 		return (ft_putnbr(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
 	return (0);
 }
 
@@ -60,6 +62,17 @@ int	ft_putnbr(int n)
 	}
 	if (n >= 10)
 		count += ft_putnbr(n / 10);
+	count += ft_putchar((n % 10) + '0');
+	return (count);
+}
+
+int	ft_putnbr_unsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 10)
+		count += ft_putnbr_unsigned(n / 10);
 	count += ft_putchar((n % 10) + '0');
 	return (count);
 }
